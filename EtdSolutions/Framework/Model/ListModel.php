5942eb7ad3653d5a34c806ffa55374a5f854fb3b
jbanety <jbanety@gmail.com>
2014-04-07 12:12:11 +0200

    cartouche

--- EtdSolutions/Framework/Model/ListModel.php
+++ EtdSolutions/Framework/Model/ListModel.php
@@ -4,7 +4,7 @@
  *
  * @version     0.0.1
  * @copyright   Copyright (C) 2014 ETD Solutions, SARL Etudoo. Tous droits réservés.
- * @license     http://etdglobalone.com/LICENSE
+ * @license     http://etd-solutions.com/LICENSE
  * @author      ETD Solutions http://etd-solutions.com
  */
 
