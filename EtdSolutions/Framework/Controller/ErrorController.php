9a32fac92faafe959d7960289ead97d280153e97
jbanety <jbanety@gmail.com>
2014-05-16 14:31:00 +0200

    Modif bug contrôle accès ErrorController

--- EtdSolutions/Framework/Controller/ErrorController.php
+++ EtdSolutions/Framework/Controller/ErrorController.php
@@ -14,10 +14,10 @@ defined('_JEXEC') or die;
 
 class ErrorController extends Controller {
 
-    public function canDo($action = null, $section = null) {
+    public function display($view = null) {
 
-        // On ne contrôle rien pour le controller des erreurs.
-        return true;
+        return $this->renderView($view);
+
     }
 
 }
