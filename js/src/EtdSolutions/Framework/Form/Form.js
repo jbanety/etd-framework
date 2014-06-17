50b20ec78a12150c4e87692a3e7f1d740e7a5601
jbanety <jbanety@gmail.com>
2014-06-17 15:08:49 +0200

    Modif JS

--- js/src/EtdSolutions/Framework/Form/Form.js
+++ js/src/EtdSolutions/Framework/Form/Form.js
@@ -9,23 +9,32 @@
 
 EtdSolutions.Framework.Form = {
 
+    $form: null,
+
     options: {
-        form: null,
         ajaxURI: null,
         token: null,
-        data: {}
+        data: {},
+        itemView: null,
+        listView: null
     },
 
-    init: function(options) {
+    init: function(form, options) {
+        this.$form = $(form);
         $.extend(true, this.options, options);
+
+        // On lie les événements
+        this.$form.find('.btn-list').on('click', $.proxy(this.onListBtnClick, this));
+        $('#limit').on('change', $.proxy(this.onLimitChange, this));
+
+        return this;
     },
 
     prepareEditRow: function(row) {
 
         row = $(row);
 
-        var self = this,
-            input = row.find('.inline-edit-input');
+        var self = this, input = row.find('.inline-edit-input');
 
         row.find('.inline-edit-btn').on('click', function(e) {
             e.preventDefault();
@@ -44,8 +53,8 @@ EtdSolutions.Framework.Form = {
             e.preventDefault();
 
             var data = {
-                'task' : 'updateField',
-                'name' : input.attr('name'),
+                'task': 'updateField',
+                'name': input.attr('name'),
                 'value': input.val()
             };
 
@@ -59,6 +68,8 @@ EtdSolutions.Framework.Form = {
 
         });
 
+        return this;
+
     },
 
     postAjax: function(data, sucCallback, errCallback) {
@@ -69,18 +80,86 @@ EtdSolutions.Framework.Form = {
         $.post(this.options.ajaxURI, data, function(data) {
             if (data.error && errCallback) {
                 errCallback(data);
-            } else if(sucCallback) {
+            } else if (sucCallback) {
                 sucCallback(data);
             }
         });
+
+        return this;
     },
 
     setAjaxURI: function(uri) {
         this.options.ajaxURI = uri;
+        return this;
     },
 
     setAdditionnalData: function(data) {
         this.options.data = data;
+        return this;
+    },
+
+    addSelectTask: function(btnId, task) {
+        var self = this;
+        $('#' + btnId).on('click', function(e) {
+            e.preventDefault();
+            if (self.$form.find('input[type=checkbox]:checked').length > 0) {
+                self.submitTask(task, self.options.itemView);
+            } else {
+                alert(EtdSolutions.Framework.Language.Text._('APP_ERROR_NO_ITEM_SELECTED'));
+            }
+        });
+        return this;
+    },
+
+    addSubmitTask: function(btnId, task, action) {
+        var self = this;
+        $('#' + btnId).on('click', function(e) {
+            e.preventDefault();
+            self.submitTask(task, action);
+        });
+        return this;
+    },
+
+    addValidationTask: function(btnId, task, action) {
+        var self = this;
+        $('#' + btnId).on('click', function(e) {
+            e.preventDefault();
+            if (self.validate()) {
+                self.submitTask(task, action);
+            } else {
+                // raise error
+            }
+        });
+        return this;
+    },
+
+    submitTask: function(task, action) {
+        if (action) {
+            this.$form.attr('action', '/' + action);
+        }
+        this.$form.find('input[name=\"task\"]').val(task);
+        this.$form.submit();
+        return this;
+    },
+
+    validate: function() {
+        return true;
+    },
+
+    onListBtnClick: function(e) {
+        e.preventDefault();
+        var target = $(e.delegateTarget), data = target.attr('href').split('/').splice(1);
+        this.$form.attr('action', '/' + data[0]);
+        this.$form.find('input[name=\"task\"]').val(data[1]);
+        this.$form.find('.list-col-cb input').prop('checked', false);
+        this.$form.find('input[type=\"checkbox\"][value=\"' + data[2] + '\"]').prop('checked', true);
+        this.$form.submit();
+    },
+
+    onLimitChange: function(e) {
+        e.preventDefault();
+        this.$form.attr('action', '/' + this.options.listView);
+        this.$form.submit();
     }
 
 };
